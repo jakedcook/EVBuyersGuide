b2d6410f4149fbb1a4dcdbdf343e468cb4ff90de
Jake Cook <jake.cook@mavs.uta.edu>
2025-03-13 15:36:59 -0500

    Updated data processing and efficiency scale calculations

---
+++ resources/vehicleLeaderBoard.sql
@@ -0,0 +1,8 @@
+INSERT INTO vehicleINFO (make, model, msrp, efficiency)
+VALUES ('Tesla', 'Model 3', 29999, 1.384),
+		('Tesla', 'Model S', 73490, 1.64),
+		('Toyota', 'Prius', 28350, 57),
+		('Toyota', 'Corolla', 22325, 35),
+		('Toyota', 'Corolla Hybrid', 23825, 50),
+		('Honda', 'Civic Hybrid', 28960, 49),
+		('Honda', 'Civic', 24250, 36);
